eea46946c4a51b57ac83282efc4a21146919d393
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2023-10-29 23:55:10 +0100

    Add hostname to config.hosts

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -23,5 +23,7 @@ module AppStream
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << "link12.ddns.net:5000"
   end
 end
